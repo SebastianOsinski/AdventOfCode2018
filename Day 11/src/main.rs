d2690a132497e2587ccc1b196fa05b4177af29fc
Sebastian Osiński <seb.osinski@gmail.com>
2018-12-11 18:59:23 +0100

    Add solution for part 2 of Day 11

--- Day 11/src/main.rs
+++ Day 11/src/main.rs
@@ -1,50 +1,58 @@
-const GRID_SERIAL_NUMBER: isize = 6878;
-const SIZE: isize = 3;
+const MIN_COORD: usize = 1;
+const MAX_COORD: usize = 300;
+const GRID_SERIAL_NUMBER: usize = 6878;
+const SIZE: usize = 3;
 
 fn main() {
-    const MIN_COORD: isize = 1;
-    const MAX_COORD: isize = 300;
-
-    // println!("{}", power_level(122, 79, 57));
-    // println!("{}", power_level(217, 196, 39));
-    // println!("{}", power_level(101, 153, 71));
-
-    let mut max_total_power_x: isize = 0;
-    let mut max_total_power_y: isize = 0;
+    let mut max_total_power_x: usize = 0;
+    let mut max_total_power_y: usize = 0;
+    let mut max_total_power_size: usize = 0;
     let mut max_total_power = isize::min_value();
 
-    for x in MIN_COORD..=(MAX_COORD + 1 - SIZE) {
-        for y in MIN_COORD..=(MAX_COORD + 1 - SIZE) {
-            let total_power = total_power_level(x, y, SIZE);
+    let mut power_levels = [[0; MAX_COORD]; MAX_COORD];
+    
+    for x in MIN_COORD..=MAX_COORD {
+        for y in MIN_COORD..=MAX_COORD {
+            power_levels[x - 1][y - 1] = power_level(x, y); 
+        }
+    }
 
-            if total_power > max_total_power {
-                max_total_power = total_power;
-                max_total_power_x = x;
-                max_total_power_y = y;
+    for size in 1..=300 {
+        println!("SIZE: {}", size);
+        for x in MIN_COORD..=(MAX_COORD + 1 - size) {
+            for y in MIN_COORD..=(MAX_COORD + 1 - size) {
+                let total_power = total_power_level(x, y, size, &power_levels);
+
+                if total_power > max_total_power {
+                    max_total_power = total_power;
+                    max_total_power_x = x;
+                    max_total_power_y = y;
+                    max_total_power_size = size;
+                }
             }
         }
     }
 
-    println!("{}, {} = {}", max_total_power_x, max_total_power_y, max_total_power);
+    println!("{},{},{} = {}", max_total_power_x, max_total_power_y, max_total_power_size, max_total_power);
 }
 
-fn total_power_level(x: isize, y: isize, size: isize) -> isize {
+fn total_power_level(x: usize, y: usize, size: usize, power_levels: &[[isize; MAX_COORD]]) -> isize {
     let mut sum = 0;
 
     for i in x..(x + size) {
         for k in y..(y + size) {
-            sum += power_level(i, k, GRID_SERIAL_NUMBER);
+            sum += power_levels[i - 1][k - 1];
         }
     }
 
     sum
 }
 
-fn power_level(x: isize, y: isize, grid_serial_number: isize) -> isize {
-    let rack_id = x + 10;
+fn power_level(x: usize, y: usize) -> isize {
+    let rack_id = (x as isize) + 10;
 
-    let power_level = rack_id * y;
-    let power_level = power_level + grid_serial_number;
+    let power_level = rack_id * y as isize;
+    let power_level = power_level + GRID_SERIAL_NUMBER as isize;
     let power_level = power_level * rack_id;
     let power_level = (power_level / 100) % 10;
 
